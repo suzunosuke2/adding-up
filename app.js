226d62ca50ac33dbee5ea7d1b8c2a94b9d4f58b2
Takashi Suzuki <taka53368230@gmail.com>
2020-09-15 06:00:39 +0900

    ランキングの実装完了

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 'use strict';
+// fs(File System) モジュールを読み込んで使えるようにする
 const fs = require('fs');
+// readline モジュールを読み込んで使えるようにする
 const readline = require('readline');
+// pop-pref.csvをファイルとして読み込める状態に準備する
 const rs = fs.createReadStream('./popu-pref.csv');
+// readline モジュールに　rs を設定する
 const rl = readline.createInterface({ input: rs, output: {} });
 const prefectureDataMap = new Map(); // key: 都道府県 value: 集計データのオブジェクト
+// popu-pref.csv のデータを1行ずつ読み込んで、設定された関数を実行する
 rl.on('line', lineString => {
   const columns = lineString.split(',');
-  const year = parseInt(columns[0]);
-  const prefecture = columns[1];
-  const popu = parseInt(columns[3]);
+  const year = parseInt(columns[0]); // 年
+  const prefecture = columns[1]; // 都道府県名
+  const popu = parseInt(columns[3]); // 15～19歳の人口
   if (year === 2010 || year === 2015) {
+    // 都道府県ごとのデータを作る
     let value = prefectureDataMap.get(prefecture);
+    // データがなかったらデータを初期化
     if (!value) {
       value = {
         popu10: 0,
@@ -28,12 +35,16 @@ rl.on('line', lineString => {
   }
 });
 rl.on('close', () => {
+    // 全データをループして変化率を計算
     for (let [key, value] of prefectureDataMap) {
       value.change = value.popu15 / value.popu10;
     }
+    // 並べ替えを行う
     const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
+      // 引き算の結果、マイナスなら降順、プラスなら昇順に入れ替え
       return pair2[1].change - pair1[1].change;
     });
+    // データを整形
     const rankingStrings = rankingArray.map(([key, value]) => {
       return (
         key +
